5c9fc9c88a26865e35d04eb1e37149b9d6d45580
huyhoang25 <ndhhoang.02052002@gmail.com>
2026-07-24 00:27:56 +0900

    init repo

---
+++ src/server/api/trpc.ts
@@ -0,0 +1,31 @@
+import { initTRPC } from "@trpc/server";
+import superjson from "superjson";
+import { ZodError, flattenError } from "zod";
+import { db } from "../db";
+
+export const createTRPCContext = async (otps: { headers: Headers }) => {
+  return {
+    db,
+    header: otps.headers,
+  };
+};
+
+const t = initTRPC.context<typeof createTRPCContext>().create({
+    transformer: superjson,
+    errorFormatter({shape, error}) {
+        return {
+          ...shape,
+          data: {
+            ...shape.data,
+            zodError:
+              error.cause instanceof ZodError
+                ? flattenError(error.cause)
+                : null,
+          },
+        };
+    }
+})
+
+export const createCallerFactory = t.createCallerFactory;
+export const createTRPCRouter = t.router;
+export const publicProcedure = t.procedure;
